6d5a780edc169d5513d9a4def7ee7ce896d75468
meokg456@bitbucket.org <meokg456@bitbucket.org>
2019-12-20 20:15:50 +0700

    + Auto next

--- MyMusic/MainWindow.xaml.cs
+++ MyMusic/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -53,6 +54,17 @@ namespace MyMusic
 			_timer = new DispatcherTimer();
 			_timer.Tick += timer_Tick;
 			_player.MediaOpened += _player_MediaOpened;
+			_player.MediaEnded += _player_MediaEnded;
+		}
+
+		private void _player_MediaEnded(object sender, EventArgs e)
+		{
+			var index = musicListBox.SelectedIndex;
+			var playlist = PlayLists.SelectedItem as PlayList;
+			index++;
+			if (index >= playlist.ItemList.Count) index = 0;
+			musicListBox.SelectedItem = playlist.ItemList[index];
+			MusicListBox_SelectionChanged(sender, e as SelectionChangedEventArgs);
 		}
 
 		private void _player_MediaOpened(object sender, EventArgs e)
@@ -125,10 +137,14 @@ namespace MyMusic
 			{
 				_timer.Stop();
 				_player.Stop();
+				_player.Close();
 				playButtonIcon.Source = _playBitmapImage;
 				_isPlaying = !_isPlaying;
 			}
 			_player.Open(new Uri(song.FullName, UriKind.Absolute));
+			while (_player.NaturalDuration.HasTimeSpan == false);
+			
+			PlayButton_Click(sender, e);
 		}
 
 		private void ProgressSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
