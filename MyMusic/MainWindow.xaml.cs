2afd53dfc28ff15bda72a223c6cae2be6977e57c
meokg456@bitbucket.org <meokg456@bitbucket.org>
2019-12-20 09:31:50 +0700

    + Slider, Play Button Design

--- MyMusic/MainWindow.xaml.cs
+++ MyMusic/MainWindow.xaml.cs
@@ -20,9 +20,30 @@ namespace MyMusic
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		bool _isPlaying = false;
+		BitmapImage _pauseBitmapImage = new BitmapImage(new Uri("Icons/pause.png", UriKind.Relative));
+		BitmapImage _playBitmapImage = new BitmapImage(new Uri("Icons/play button.png", UriKind.Relative));
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			
+		}
+
+		private void PlayButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (_isPlaying == false)
+			{
+				playButtonIcon.Source = _pauseBitmapImage;
+			}
+			else
+			{
+				playButtonIcon.Source = _playBitmapImage;
+			}
+			_isPlaying = !_isPlaying;
 		}
 	}
 }
