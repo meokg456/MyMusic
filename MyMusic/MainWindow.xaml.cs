0cfa72d64bafe50285677a7b2f1482e7ae17b85e
meokg456@bitbucket.org <meokg456@bitbucket.org>
2019-12-20 20:51:42 +0700

    Adjust layout

--- MyMusic/MainWindow.xaml.cs
+++ MyMusic/MainWindow.xaml.cs
@@ -1,21 +1,10 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace MyMusic
@@ -38,6 +27,7 @@ namespace MyMusic
 		{
 			public string PlayListName { get; set; }
 			public BindingList<FileInfo> ItemList;
+			
 		}
 
 		BindingList<PlayList> _listPlay = new BindingList<PlayList>();
