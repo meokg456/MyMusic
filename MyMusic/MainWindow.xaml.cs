70fc93c81a44fd1240c34e30b9f2d730d2be2878
meokg456@bitbucket.org <meokg456@bitbucket.org>
2019-12-21 17:30:02 +0700

    Fixbug

--- MyMusic/MainWindow.xaml.cs
+++ MyMusic/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
