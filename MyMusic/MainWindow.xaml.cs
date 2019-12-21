da6b6384927c8038425acb49d8fbc975fa976bb1
meokg456@bitbucket.org <meokg456@bitbucket.org>
2019-12-21 10:24:14 +0700

    Adjust layout + Mute, Volume adjustment

--- MyMusic/MainWindow.xaml.cs
+++ MyMusic/MainWindow.xaml.cs
@@ -17,13 +17,22 @@ namespace MyMusic
 		bool _isPlaying = false;
 		BitmapImage _pauseBitmapImage = new BitmapImage(new Uri("Icons/pause.png", UriKind.Relative));
 		BitmapImage _playBitmapImage = new BitmapImage(new Uri("Icons/play button.png", UriKind.Relative));
+		BitmapImage _muteBitmapImage = new BitmapImage(new Uri("Icons/mute.png", UriKind.Relative));
+		BitmapImage _volumeBitmapImage = new BitmapImage(new Uri("Icons/volume.png", UriKind.Relative));
 		MediaPlayer _player = new MediaPlayer();
 		DispatcherTimer _timer;
-        int optionRepeat = -1;
+        RepeatOption repeatOption = RepeatOption.NoRepeat;
 
         public MainWindow()
 		{
 			InitializeComponent();
+		}
+		public enum RepeatOption
+		{
+			NoRepeat = -1,
+			SelfRepeat = 0,
+			SequenceRepeat = 1,
+			RandomRepeat = 2,
 		}
 
         
@@ -49,6 +58,8 @@ namespace MyMusic
 			_timer.Tick += timer_Tick;
 			_player.MediaOpened += _player_MediaOpened;
 			_player.MediaEnded += _player_MediaEnded;
+			volumeSlider.Value = 50;
+			_player.Volume = 0.5;
 		}
 
 		private void _player_MediaEnded(object sender, EventArgs e)
@@ -56,19 +67,27 @@ namespace MyMusic
             var index = musicListBox.SelectedIndex;
             var playlist = PlayLists.SelectedItem as PlayList;
             var count = playlist.ItemList.Count;
-
-            if (optionRepeat == 0)
+			if (repeatOption == RepeatOption.NoRepeat)
+			{
+				playButtonIcon.Source = _playBitmapImage;
+				_player.Pause();
+				_timer.Stop();
+				_isPlaying = false;
+				progressSlider.Value = 0;
+				ProgressSlider_ValueChanged(sender, e as RoutedPropertyChangedEventArgs<double>);
+			}
+            if (repeatOption == RepeatOption.SelfRepeat)
             {
                 musicListBox.SelectedItem = playlist.ItemList[index];
                 MusicListBox_SelectionChanged(sender, e as SelectionChangedEventArgs);
             }
-            else if (optionRepeat == 1)
+            else if (repeatOption == RepeatOption.SequenceRepeat)
             {
                 index = (index + 1) % count;
                 musicListBox.SelectedItem = playlist.ItemList[index];
                 MusicListBox_SelectionChanged(sender, e as SelectionChangedEventArgs);
             }
-            else if (optionRepeat == 2)
+            else if (repeatOption == RepeatOption.RandomRepeat)
             {
                 var oldIndex = index;
 
@@ -180,17 +199,70 @@ namespace MyMusic
 
         private void SelfRepeat_Click(object sender, RoutedEventArgs e)
         {
-            optionRepeat = 0;
+			var button = sender as Button;
+			if(button.Tag as string != "chose")
+			{
+				repeatOption = RepeatOption.SelfRepeat;
+				button.Tag = "chose";
+				sequenRepeat.Tag = "";
+				randomRepeat.Tag = "";
+			}
+			else
+			{
+				repeatOption = RepeatOption.NoRepeat;
+				button.Tag = "";
+			}
         }
 
         private void SequenRepeat_Click(object sender, RoutedEventArgs e)
         {
-            optionRepeat = 1;
-        }
+			var button = sender as Button;
+			if (button.Tag as string != "chose")
+			{
+				repeatOption = RepeatOption.SequenceRepeat;
+				button.Tag = "chose";
+				selfRepeat.Tag = "";
+				randomRepeat.Tag = "";
+			}
+			else
+			{
+				repeatOption = RepeatOption.NoRepeat;
+				button.Tag = "";
+			}
+		}
 
         private void RandomRepeat_Click(object sender, RoutedEventArgs e)
         {
-            optionRepeat = 2;
-        }
-    }
+			var button = sender as Button;
+			if (button.Tag as string != "chose")
+			{
+				repeatOption = RepeatOption.RandomRepeat;
+				button.Tag = "chose";
+				sequenRepeat.Tag = "";
+				selfRepeat.Tag = "";
+			}
+			else
+			{
+				repeatOption = RepeatOption.NoRepeat;
+				button.Tag = "";
+			}
+		}
+		private void MuteButton_Click(object sender, RoutedEventArgs e)
+		{
+			_player.IsMuted = !_player.IsMuted;
+			if(_player.IsMuted == true)
+			{
+				muteButtonIcon.Source = _muteBitmapImage;
+			}
+			else
+			{
+				muteButtonIcon.Source = _volumeBitmapImage;
+			}
+		}
+
+		private void MuteSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			_player.Volume = volumeSlider.Value / volumeSlider.Maximum;
+		}
+	}
 }
